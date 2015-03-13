13961b8569491335740bb12249671af52c726504
ehnree <hzhou1995@gmail.com>
2015-03-12 22:09:09 -0400

    Fixed CSS error( #map_canvas -> #map-canvas). Assured that map initialization occurs after function success() is run. Added marker to current location. Next goal: work on XML commands

--- mmap/mmap.js
+++ mmap/mmap.js
@@ -2,24 +2,16 @@
 // Written by: Henry Zhou
 // Date: 3/6/2015
 
-
 /* Initialize vars and objects (Google map) */ 
 
-var latitude;
-var longitude; 
+var latitude; //= 42.407455;
+var longitude; //= -71.1208;
+var login = "BenJohnson";
 
 function initialize() {
 	console.log("in initialize");
-	findMe();
-	console.log("finished findMe");
 
-	var mapOptions = {
-		zoom: 8,
-		center: new google.maps.LatLng(latitude, longitude)
-	};
 
-	var map = new google.maps.Map(document.getElementById("map-canvas"),
-		mapOptions);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
@@ -38,18 +30,46 @@ function findMe() {
 		longitude = position.coords.longitude;
 		console.log("Lat: " + latitude);
 		console.log("Long: " + longitude);
+		console.log("finished findMe");
+		var mapOptions = {
+		zoom: 18,
+		center: new google.maps.LatLng(latitude, longitude)
+	};
+
+	myLatlng = new google.maps.LatLng(latitude, longitude);
+	var map = new google.maps.Map(document.getElementById("map-canvas"),
+		mapOptions);
+	console.log("map created");
+
+	var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  	});
+  	
+	sendMyLocation();
 	}
 
 	function error() {
     	console.log("Unable to retrieve your location");
     };
-
 }
 
-/* retrieve current information */
+/* send my location to database */
 
-/* send information to database */
+function sendMyLocation(){
+	request = new XMLHttpRequest();
+	url = "https://secret-about-box.herokuapp.com/sendLocation";
+	params = "login=" + login + "&lat=" + latitude + "&lng=" + longitude;
+	console.log(params);
+	request.open("POST", url, true);
 
-/* retrive locations of people in the class on the map */
+	//Send request w/ proper header info 
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+	request.send(params);
+}
+
+/* retrieve locations of people in the class on the map */
 
 /* Display previous information */ 
